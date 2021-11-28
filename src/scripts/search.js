44efab113f476ba222405f6ae8547ffb3eec2011
PressJump <TUOBCAN@users.noreply.github.com>
2021-11-28 19:05:23 +1100

    🚧 SearchAPI
    - SearchAPI

---
+++ src/scripts/search.js
@@ -0,0 +1,28 @@
+const button = document.getElementById("search");
+
+button.addEventListener("click", () => {
+    if(document.getElementById("search-input").value != "") {
+        search();
+    }
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && document.activeElement.id === "search-input" && document.activeElement.value !== "") {
+        search();
+    }
+});
+
+async function search() {
+    var tabID = await getCurrent();
+    var search_input = document.getElementById("search-input").value;
+    browser.search.search({
+        query: search_input,
+        tabId: tabID
+    });
+    
+}
+  
+async function getCurrent() {
+    return (await browser.tabs.getCurrent()).id
+}
+
