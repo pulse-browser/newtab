e6fb5dcc63480406f40e63e4a0c4e03f81cb790b
PressJump <TUOBCAN@users.noreply.github.com>
2021-11-28 19:49:35 +1100

    🎨 Formatting Improvement

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,28 +1,26 @@
 const button = document.getElementById("search");
 
 button.addEventListener("click", () => {
-    if(document.getElementById("search-input").value != "") {
-        search();
-    }
+  if (document.getElementById("search-input").value != "") {
+    search();
+  }
 });
 
 document.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" && document.activeElement.id === "search-input" && document.activeElement.value !== "") {
-        search();
-    }
+  if (e.key === "Enter" && document.activeElement.id === "search-input" && document.activeElement.value !== "") {
+    search();
+  }
 });
 
 async function search() {
-    var tabID = await getCurrent();
-    var search_input = document.getElementById("search-input").value;
-    browser.search.search({
-        query: search_input,
-        tabId: tabID
-    });
-    
-}
-  
-async function getCurrent() {
-    return (await browser.tabs.getCurrent()).id
+  var tabID = await getCurrent();
+  var search_input = document.getElementById("search-input").value;
+  browser.search.search({
+    query: search_input,
+    tabId: tabID,
+  });
 }
 
+async function getCurrent() {
+  return (await browser.tabs.getCurrent()).id;
+}
