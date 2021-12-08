88f55efb894d00696608d414f9c8b6f9eb9f4ecf
trickypr <trickypr@icloud.com>
2021-12-08 19:00:21 +1100

    💄 Focus style

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,20 +1,24 @@
 const button = document.getElementById('search')
+const searchBox = document.getElementById('searchbox')
+const searchInput = document.getElementById('searchInput')
+
+searchBox.addEventListener('click', (e) => searchInput.focus())
 
 button.addEventListener('click', () => {
-  if (document.getElementById('search-input').value != '') {
+  if (document.getElementById('searchInput').value != '') {
     search()
   }
 })
 
 document.addEventListener('keydown', (e) => {
-  if (e.key === 'Enter' && document.activeElement.id === 'search-input' && document.activeElement.value !== '') {
+  if (e.key === 'Enter' && document.activeElement.id === 'searchInput' && document.activeElement.value !== '') {
     search()
   }
 })
 
 async function search() {
   var tabID = await getCurrent()
-  var search_input = document.getElementById('search-input').value
+  var search_input = document.getElementById('searchInput').value
   browser.search.search({
     query: search_input,
     tabId: tabID,
