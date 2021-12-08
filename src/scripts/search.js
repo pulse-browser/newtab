bddddcbc8128d13a9a4514fa846720099caa0e63
trickypr <trickypr@icloud.com>
2021-12-08 18:38:14 +1100

    🎨 Use Fushra code style

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,26 +1,26 @@
-const button = document.getElementById("search");
+const button = document.getElementById('search')
 
-button.addEventListener("click", () => {
-  if (document.getElementById("search-input").value != "") {
-    search();
+button.addEventListener('click', () => {
+  if (document.getElementById('search-input').value != '') {
+    search()
   }
-});
+})
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Enter" && document.activeElement.id === "search-input" && document.activeElement.value !== "") {
-    search();
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter' && document.activeElement.id === 'search-input' && document.activeElement.value !== '') {
+    search()
   }
-});
+})
 
 async function search() {
-  var tabID = await getCurrent();
-  var search_input = document.getElementById("search-input").value;
+  var tabID = await getCurrent()
+  var search_input = document.getElementById('search-input').value
   browser.search.search({
     query: search_input,
     tabId: tabID,
-  });
+  })
 }
 
 async function getCurrent() {
-  return (await browser.tabs.getCurrent()).id;
+  return (await browser.tabs.getCurrent()).id
 }
